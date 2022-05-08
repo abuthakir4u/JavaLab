d67899488ed9a82d87f250a4e4598817dc2c69e3
Abuthakir <abuthakir4u@gmail.com>
2022-05-08 00:08:30 -0400

    examples added

---
+++ CoreJava/src/main/java/org/javalab/jdbc/JdbcExample1.java
@@ -0,0 +1,21 @@
+package org.javalab.jdbc;
+
+import java.sql.*;
+
+public class JdbcExample1 {
+    public static void main(String[] args) throws ClassNotFoundException, SQLException {
+        Class.forName("com.mysql.cj.jdbc.Driver");
+        String url = "jdbc:mysql://localhost:3306/sakila"; //sakila is database name
+        String username = "root";
+        String password = "Bot$%^01";
+        String query = "select * from actor where actor_id = ?";
+        Connection con = DriverManager.getConnection(url, username, password);
+        PreparedStatement st = con.prepareStatement(query);
+        st.setInt(1, 200);
+        ResultSet rs  = st.executeQuery();//should not pass query here
+        rs.next();
+        System.out.println(rs.getInt(1));
+        st.close();
+        con.close();
+    }
+}
---
+++ CoreJava/src/main/java/org/javalab/jdbc/JdbcExample2.java
@@ -0,0 +1,68 @@
+package org.javalab.jdbc;
+
+import java.sql.*;
+
+public class JdbcExample2 {
+    public static void main(String[] args) {
+        StudentDAO dao = new StudentDAO();
+        //Adding student into table
+        dao.connect();
+        Student s = new Student();
+        s.rollno = 10;
+        s.sname = "Igsat";
+        dao.addStudent(s);
+
+        //Reading a student from table
+        dao.connect();
+        Student s1 = dao.getStudent(10);
+        System.out.println(s1.sname);
+    }
+}
+
+class StudentDAO {
+    Connection con = null;
+    public void connect() {
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            String url = "jdbc:mysql://localhost:3306/sakila"; //sakila is database name
+            String username = "root";
+            String password = "Bot$%^01";
+            this.con = DriverManager.getConnection(url, username, password);
+        } catch (Exception e) {
+            System.out.println("Exception occurred: " + e);
+        }
+    }
+    public Student getStudent(int rollno) {
+        Student s = new Student();
+        s.rollno = rollno;
+        String query = "select rollno, sname from student where rollno = ?";
+        try {
+            PreparedStatement st = con.prepareStatement(query);
+            st.setInt(1, rollno);
+            ResultSet rs = st.executeQuery();//should not pass query here
+            rs.next();
+            s.sname = rs.getString(2);
+            st.close();
+            con.close();
+        } catch (Exception e) {
+            System.out.println("Exception occurred: " + e);
+        }
+        return s;
+    }
+    public void addStudent(Student s) {
+        String query = "insert into student values (?, ?)";
+        try {
+            PreparedStatement st = con.prepareStatement(query);
+            st.setInt(1, s.rollno);
+            st.setString(2, s.sname);
+            int result = st.executeUpdate();
+            System.out.println("Affected rows: " + result);
+        } catch (Exception e) {
+            System.out.println("Exception occurred: " + e);
+        }
+    }
+}
+class Student {
+    int rollno;
+    String sname;
+}
