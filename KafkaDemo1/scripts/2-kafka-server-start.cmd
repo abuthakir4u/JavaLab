ab620162b81a3f65d476af200dc63e0e836ebdf6
Abuthakir <abuthakir4u@gmail.com>
2023-03-04 13:19:38 -0500

    examples added

---
+++ KafkaDemo1/scripts/2-kafka-server-start.cmd
@@ -0,0 +1,2 @@
+set KAFKA_HOME=C:\Abuthakir\Softwares\confluent-7.3.1
+%KAFKA_HOME%\bin\windows\kafka-server-start.bat %KAFKA_HOME%\etc\kafka\server-2.properties
---
+++ KafkaDemo1/scripts/describe-topic.cmd
@@ -0,0 +1,2 @@
+set KAFKA_HOME=C:\Abuthakir\Softwares\confluent-7.3.1
+%KAFKA_HOME%\bin\windows\kafka-topics.bat --describe  --bootstrap-server localhost:9092 --topic invoice
